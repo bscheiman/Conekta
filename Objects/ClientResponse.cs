188db473e305b2e7428d1a4135a5b6d5559cb17a
Boris Scheiman <bscheiman@gmail.com>
2014-10-04 21:59:16 -0500

    Re-QF

--- Objects/ClientResponse.cs
+++ Objects/ClientResponse.cs
@@ -6,6 +6,10 @@ using System.Runtime.Serialization;
 namespace Conekta.Objects {
     [DataContract]
     public class ClientResponse {
+        // TODO: FIX/Refactor
+        [DataMember(Name = "billing_address")]
+        public string BillingAddress { get; internal set; }
+
         [DataMember(Name = "cards")]
         public Card[] Cards { get; set; }
 
@@ -14,6 +18,9 @@ namespace Conekta.Objects {
 
         [DataMember(Name = "deleted")]
         public bool Deleted { get; internal set; }
+
+        [DataMember(Name = "email")]
+        public string Email { get; internal set; }
 
         public static ClientResponse Empty {
             get {
@@ -28,6 +35,18 @@ namespace Conekta.Objects {
 
         [DataMember(Name = "livemode")]
         public bool Livemode { get; internal set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; internal set; }
+
+        [DataMember(Name = "phone")]
+        public string Phone { get; internal set; }
+
+        [DataMember(Name = "plan")]
+        public string Plan { get; internal set; }
+
+        [DataMember(Name = "shipping_address")]
+        public string ShippingAddress { get; internal set; }
 
         [DataMember(Name = "subscription")]
         public Subscription Subscription { get; set; }
