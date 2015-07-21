c00bd7488cb04ce33f6a772b0cd88c5e1410e581
Boris Scheiman <bscheiman@gmail.com>
2015-07-20 21:38:48 -0500

    build.bat

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ REM Package restore
 tools\nuget.exe restore Conekta.sln -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Conekta.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Conekta.sln /t:Clean,Build /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir artifacts
@@ -21,8 +21,13 @@ mkdir artifacts\nuget
 tools\nuget.exe pack Conekta.nuspec -symbols -o artifacts\nuget -p Configuration=%config% %version%
 
 REM Plain assemblies
+
+rd /s /q artifacts
 mkdir artifacts\assemblies
 mkdir artifacts\assemblies\net40
 mkdir artifacts\assemblies\net45
+
 copy Conekta.NET40\bin\%config%\Conekta*.dll artifacts\assemblies\net40
+copy Conekta.NET40\bin\%config%\Conekta*.pdb artifacts\assemblies\net40
+copy Conekta.NET45\bin\%config%\Conekta*.dll artifacts\assemblies\net45
 copy Conekta.NET45\bin\%config%\Conekta*.pdb artifacts\assemblies\net45
